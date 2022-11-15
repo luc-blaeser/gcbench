044e50e9afe58f78f358764fd89cc2cb3649902e
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-15 10:26:51 +0100

    update measurements

---
+++ measure-special.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd util
+cargo build --release
+if [ $? != 0 ]
+then
+    cd ..
+    echo "Failed to build the report tool"
+    exit 1
+fi
+cd ..
+GC_MODES="generational"
+PERFORMANCE_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
+for name in $PERFORMANCE_CASES
+do
+    for gc in $GC_MODES
+    do
+        ./performance.sh $gc $name
+    done    
+done
+LIMIT_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree"
+for name in $LIMIT_CASES
+do
+    for gc in $GC_MODES
+    do
+        ./limit.sh $gc $name 
+    done
+done
+cp style.css reports/
+util/target/release/report summary reports/
