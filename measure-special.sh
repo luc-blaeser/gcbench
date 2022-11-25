6006ae7d8afecb62132d126e2755bf3d31bc49aa
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-25 09:20:29 +0100

    Update measure-special.sh

--- measure-special.sh
+++ measure-special.sh
@@ -9,8 +9,8 @@ then
     exit 1
 fi
 cd ..
-GC_MODES="incremental"
-PERFORMANCE_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
+GC_MODES="generational"
+PERFORMANCE_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
 for name in $PERFORMANCE_CASES
 do
     for gc in $GC_MODES
@@ -18,7 +18,7 @@ do
         ./performance.sh $gc $name
     done    
 done
-LIMIT_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree"
+LIMIT_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree btree-map"
 for name in $LIMIT_CASES
 do
     for gc in $GC_MODES
