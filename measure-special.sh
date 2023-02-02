badef383064045fc2d643688077d6c2fb2c7fc9d
luc-blaeser <luc.blaeser@dfinity.org>
2023-02-02 15:58:41 +0100

    Update scripts

--- measure-special.sh
+++ measure-special.sh
@@ -10,7 +10,7 @@ then
 fi
 cd ..
 GC_MODES="incremental"
-PERFORMANCE_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
+PERFORMANCE_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
 for name in $PERFORMANCE_CASES
 do
     for gc in $GC_MODES
@@ -18,7 +18,7 @@ do
         ./performance.sh $gc $name
     done    
 done
-LIMIT_CASES="linked-list buffer rb-tree trie-map blobs imperative-rb-tree btree-map"
+LIMIT_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map"
 for name in $LIMIT_CASES
 do
     for gc in $GC_MODES
