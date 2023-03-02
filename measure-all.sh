be827cf2a5dd59e604a6483df21cb33d60ff68f4
Kento Sugama <kento.sugama@dfinity.org>
2023-03-01 16:40:42 -0800

    udated reports (some limit tests failed)

--- measure-all.sh
+++ measure-all.sh
@@ -9,21 +9,21 @@ then
     exit 1
 fi
 cd ..
-GC_MODES="compacting copying no generational incremental"
+GC_MODES="compacting copying generational"
 PERFORMANCE_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
 for name in $PERFORMANCE_CASES
 do
     for gc in $GC_MODES
     do
         ./performance.sh $gc $name
-    done    
+    done
 done
 LIMIT_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map"
 for name in $LIMIT_CASES
 do
     for gc in $GC_MODES
     do
-        ./limit.sh $gc $name 
+        ./limit.sh $gc $name
     done
 done
 cp style.css reports/
