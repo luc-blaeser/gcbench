5aba8427cd3b2f1d774dee00696a804b37a4cbed
luc-blaeser <luc.blaeser@dfinity.org>
2023-08-22 10:47:45 +0200

    Update measurements

--- measure-special.sh
+++ measure-special.sh
@@ -9,7 +9,7 @@ then
     exit 1
 fi
 cd ..
-GC_MODES="incremental"
+GC_MODES="no"
 PERFORMANCE_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
 for name in $PERFORMANCE_CASES
 do
