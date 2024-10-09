331d3668cd5eef9660d7f122a81901e4fc023342
luc-blaeser <luc.blaeser@dfinity.org>
2024-10-09 13:05:55 +0200

    Update copying and incremental GC benchmark results

--- measure-special.sh
+++ measure-special.sh
@@ -9,8 +9,9 @@ then
     exit 1
 fi
 cd ..
-GC_MODES="no"
-PERFORMANCE_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
+GC_MODES="incremental"
+PERFORMANCE_CASES="graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
+#PERFORMANCE_CASES="linked-list buffer scalable-buffer rb-tree trie-map blobs imperative-rb-tree btree-map graph random-maze game-of-life extendable-token asset-storage qr-code reversi sha256 cancan"
 for name in $PERFORMANCE_CASES
 do
     for gc in $GC_MODES
